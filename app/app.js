76c0a43b9018c8311260e6f8cc7fe8ddf962a9aa
renxiao <ckintense@gmail.com>
2015-04-04 18:48:17 +0900

    Bootstrap, navbar, etc

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngMessages',
   'myApp.view1',
   'myApp.view2',
+  'myApp.signin',
+  'myApp.signup',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
